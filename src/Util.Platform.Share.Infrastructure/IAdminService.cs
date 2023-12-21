1672cc1054388e70b6593d6f9a4d4a86587ee77e
root <8907060@qq.com>
2023-12-21 15:49:28 +0800

    更新nuget版本

---
+++ src/Util.Platform.Share.Infrastructure/IAdminService.cs
@@ -0,0 +1,19 @@
+﻿namespace Util.Platform.Share;
+
+/// <summary>
+/// 管理员服务
+/// </summary>
+public interface IAdminService : IAdminService<Guid> {
+}
+
+/// <summary>
+/// 管理员服务
+/// </summary>
+public interface IAdminService<in TUserId> : IScopeDependency {
+    /// <summary>
+    /// 是否管理员
+    /// </summary>
+    /// <param name="userId">用户标识</param>
+    /// <param name="token">取消令牌</param>
+    Task<bool> IsAdminAsync( TUserId userId, CancellationToken token = default );
+}
